fc799d69bd131b5c911eb5a29a767511fde3a24b
djairmaykon <djairmaykon@gmail.com>
2022-05-13 08:33:43 -0300

    feat: função de chamada da api do covid

---
+++ src/service/api.ts
@@ -0,0 +1,15 @@
+const path = 'https://coronavirus-19-api.herokuapp.com/countries'
+
+const headers: RequestInit = {
+  method: 'get',
+  mode: 'cors',
+  cache: 'default',
+}
+
+function getCountry(country: string) {
+  return fetch(`${path}/${country}`, headers).then(response => response.json())
+}
+
+export default {
+  getCountry,
+}
